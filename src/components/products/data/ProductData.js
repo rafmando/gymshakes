4e319075033062874f10db2843cfb77592633e38
rafmando <rafmando1111@gmail.com>
2021-07-31 16:37:01 +0100

    Add product data

---
+++ src/components/products/data/ProductData.js
@@ -0,0 +1,8 @@
+export const data = [
+    {id:1, name: 'Optimum Nutrition Gold ', price: 30.99,  image:'/images/protein1.png'},
+    {id:2, name: 'ProJym', price: 25.31, image:'/images/protein2.png'},
+    {id:3, name: 'Signature Whey', price: 19.52, image:'/images/protein3.jpeg'},
+    {id:4, name: 'Nitro Tech', price: 24.82, image:'/images/protein4.png'},
+    {id:5, name: 'Dymatize (Pebble flavour)', price: 20.97, image:'/images/protein5.png'},
+    {id:6, name: 'Remix Nutrition', price: 21.69, image:'/images/protein6.jpeg'},
+]
