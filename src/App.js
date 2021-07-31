ae9d7effa797afc1ef7e34009c9f8ed13db140ec
rafmando <rafmando1111@gmail.com>
2021-07-31 22:15:02 +0100

    Import products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Products from './components/products/Products'
 
 function App() {
   return (
    <>
     <Header/>
+    <Products/>
    </>
   );
 }
